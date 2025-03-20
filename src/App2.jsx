2233b5989d73170f29b08205d5d26063bf49c6e9
Arman Merdzanic <merdzanicarman@gmail.com>
2025-03-20 11:48:45 +0100

    Updejtovan kod

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App2.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App2.css";
 
 function App2() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Ovo sam ja pravio</h1>
+      </header>
+      <section className="slike">
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </section>
+      <footer>
+        <h2>Ovo je bilo obično testiranje</h2>
+        <img src="" alt="" />
+        <img src="" alt="" />
+      </footer>
     </>
-  )
+  );
 }
 
-export default App2
+export default App2;
